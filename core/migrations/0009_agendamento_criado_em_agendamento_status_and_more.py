4b6298051a2a1c4b14c81a21bb819e3e45b7c008
danielhenrique090109 <danielhenrique090109@gmail.com>
2026-07-06 15:43:59 -0300

    fix: remove default invalido no campo criado_em do agendamento

--- core/migrations/0009_agendamento_criado_em_agendamento_status_and_more.py
+++ core/migrations/0009_agendamento_criado_em_agendamento_status_and_more.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='agendamento',
             name='criado_em',
-            field=models.DateTimeField(auto_now_add=True, default=0),
+            field=models.DateTimeField(auto_now_add=True),
             preserve_default=False,
         ),
         migrations.AddField(
